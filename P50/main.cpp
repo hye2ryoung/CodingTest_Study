2c0b93f53fdd1594541bfc0373872bd69396a34e
hyeryoung <kimhyeryoung@gimhyelyeong-ui-MacBookPro.local>
2021-09-14 20:08:55 +0900

    210914

--- P50/main.cpp
+++ P50/main.cpp
@@ -8,9 +8,40 @@
 
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 //영지(territory) 선택 (small : 2차원 배열 브루트포스)
+int a[51][51];
 int main(int argc, const char * argv[]) {
+    int h, w, n, m, i, j, k, s, sum=0, max=-2147000000;
+    scanf("%d %d", &h, &w);//전체영지 세로,가로길이
+    for(i=1; i<=h; i++){
+        for(j=1; j<=w; j++){
+            scanf("%d", &a[i][j]);//오렌지 나무 개수 정보 입력
+        }
+    }
+    scanf("%d %d", &n, &m);//현수가 하사받을 영지크기 세로,가로길이
     
+    //4중for문을 통해 나무개수를 모두 세어준다
+    //1,1꼭지점부터 끝꼭지점까지 전부 세어줄필요는 없으므로 하사받는영지크기의 마지막범위 시작점까지만 탐색
+    //(전체높이 h) - (현수영지 세로 n) + 1
+    //(전체너비 w) - (현수영지 가로 m) + 1
+    for(i=1; i<=h-n+1; i++){
+        for(j=1; j<=w-m+1; j++){
+            sum=0;
+            //현수영지의 나무개수 탐색
+            //시작점i부터 현수영지세로n을 더한지점 전까지,
+            //시작지점j부터 현수영지가로m을 더한지점 전까지 탐색
+            for(k=i; k<i+n; k++){
+                for(s=j; s<j+m; s++){
+                    sum=sum+a[k][s];//영지에있는 나무개수의 합 sum
+                }
+            }
+            if(sum>max){
+                max=sum; //나무개수의 최대개수 구하기
+            }
+        }
+    }
+    printf("%d\n", max);
     return 0;
 }
--- P51/main.cpp
+++ P51/main.cpp
@@ -6,10 +6,36 @@
 //  Copyright © 2021 hyeryoung. All rights reserved.
 //
 
-#include <iostream>
-
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//영지(territory)선택 : (large)
+//영지범위가 높이 h는 1이상,너비 w는 700이하로 확장
+int a[701][701], dy[701][701];//원 배열 a와 다이나믹 배열 dy를 전역변수로 잡아준다.
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int h, w, n, m, i, j, tmp, max=-2147000000;
+    scanf("%d %d", &h, &w);//전체영지 세로,가로길이
+    for(i=1; i<=h; i++){
+        for(j=1; j<=w; j++){
+            scanf("%d", &a[i][j]);//오렌지 나무 개수 정보 입력
+            
+            //특정지점의 누적값(나무개수) 구하는 방법
+            //특정지점의 각각 행-1, 열-1한 지점의 누적값을 더하고, 두 지점의 겹치는 영역(중복)을 빼준다음 특정지점을 더하면 그 지점의 누적값을 구할 수 있다.
+            dy[i][j]=dy[i-1][j]+dy[i][j-1]-dy[i-1][j-1]+a[i][j];
+        }
+    }
+    scanf("%d %d", &n, &m);//현수가 하사받을 영지크기 세로,가로길이
+    
+    //2중for문
+    //small문제는 시작지점으로 영역을 계산했다면, large 문제는 영역의 누적값(종료지점)으로 접근한다.
+    for(i=n; i<=h; i++){
+        for(j=m; j<=w; j++){
+            //답으로 구하고자하는 영역의 누적값(종료지점)에서 영지가로길이(m)과 세로길이(n)를 각각 뺏을때의 각각의 누적값(종료지점)을 뺀 다음, 빼준 두 영역의 겹치는 영역(중복)의 누적값(종료지점)을 한번 더해준 값이 구하고자하는 영역의 나무개수이다.
+            tmp=dy[i][j]-dy[i-n][j]-dy[i][j-m]+dy[i-n][j-m];
+            if(tmp>max) max=tmp;
+        }
+    }
+    printf("%d\n",max);
     return 0;
 }
