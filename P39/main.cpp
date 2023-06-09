710be4c11350923cc86510767c8f0c94f955bdeb
hye2ryoung <xoxo4886@gmail.com>
2023-06-09 11:37:13 +0900

    fin

--- P39/main.cpp
+++ P39/main.cpp
@@ -8,20 +8,21 @@
 
 #include <stdio.h>
 #include <vector>
+using namespace std;
 //병합정렬
 int main(int argc, const char * argv[]) {
     int n, m, i, p1=0, p2=0, p3=0;
     scanf("%d", &n);
-    std::vector<int> a(n);
+    vector<int> a(n);
     for(i=0;i<n;i++){
         scanf("%d", &a[i]); //a배열과
     }
     scanf("%d", &m);
-    std::vector<int> b(m);
+    vector<int> b(m);
     for(i=0;i<m;i++){
         scanf("%d", &b[i]); //b배열 입력
     }
-    std::vector<int> c(n+m); //c배열은 a배열과 b배열의 크기를 합친 배열
+    vector<int> c(n+m); //c배열은 a배열과 b배열의 크기를 합친 배열
     
     while(p1<n && p2<m){//a배열의 크기n과 b배열의 크기m 둘 중 크기가 작은것까지 while문이 돌 때
         if(a[p1]<b[p2]){//p1이 가리키는 수가 p2보다 작으면
@@ -38,4 +39,5 @@ int main(int argc, const char * argv[]) {
         printf("%d ",c[i]); //병합정렬된 c배열 출력
     }
     return 0;
+    
 }
