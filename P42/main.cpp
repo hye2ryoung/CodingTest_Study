85e6b23ea653917ed987aaaa32887e633ac1e18c
hye2ryoung <xoxo4886@gmail.com>
2023-06-15 22:30:45 +0900

    fin

--- P42/main.cpp
+++ P42/main.cpp
@@ -9,14 +9,16 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+using namespace std;
 //이분검색(이진탐색)
 //데이터가 정렬돼 있는 배열에서 특정한 값을 찾아내는 알고리즘
 //배열의 중간에 있는 임의의 값을 선택하여 찾고자 하는 값 X와 비교한다. X가 중간 값보다 작으면 중간 값을 기준으로 좌측의 데이터들을 대상으로, X가 중간값보다 크면 배열의 우측을 대상으로 다시 탐색한다.
 //동일한 방법으로 다시 중간의 값을 임의로 선택하고 비교한다. 해당 값을 찾을 때까지 이 과정을 반복한다.
 int main(int argc, const char * argv[]) {
+    
     int n, m, i, mid, lt=0, rt; //lt : 맨처음값으로 초기화 0번인덱스
     scanf("%d %d",&n,&m);
-    std::vector<int> a(n);
+    vector<int> a(n);
     for(i=0; i<n; i++){
         scanf("%d",&a[i]);
     }
@@ -33,4 +35,5 @@ int main(int argc, const char * argv[]) {
         else if(a[mid]<m) lt=mid+1;
     }
     return 0;
+     
 }
--- P43/main.cpp
+++ P43/main.cpp
@@ -10,6 +10,7 @@
 //이분검색 응용 : 결정 알고리즘
 int a[1001], n;//n : dvd에 들어있는 곡의 총 개수
 
+/*
 int Count(int size){//size: dvd용량(한개당)
     int i, cnt=1, sum=0;
     for(i=1;i<=n;i++){
@@ -40,4 +41,35 @@ int main(int argc, const char * argv[]) {
     }
     printf("%d\n", res);//최소용량 출력
     return 0;
+ */
+
+int Count(int size){
+    int i, cnt=1, sum=0;
+    for(i=1;i<=n;i++){
+        if(sum+a[i]>size){
+            cnt++;
+            sum=a[i];
+        }
+        else sum=sum+a[i];
+    }
+    return cnt;
+}
+
+int main(int argc, const char * argv[]) {
+    int m, i, lt=1, rt=0, mid, res, max=-2147000000;
+    scanf("%d %d", &n, &m);
+    for(i=1;i<=n;i++){
+        scanf("%d", &a[i]);
+        rt=rt+a[i];
+        if(a[i]>max) max=a[i];
+    }
+    while(lt<=rt){
+        mid=(lt+rt)/2;
+        if(mid>=max && Count(mid)<=m){
+            res=mid;
+            rt=mid-1;
+        }
+        else lt=mid+1;
+    }
+    printf("%d\n",res);
 }
